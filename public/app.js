93be2194507c267f549485b653825f00fc7eb5a6
savannalever <savannalever@icloud.com>
2019-10-21 21:41:10 -0400

    added notes

---
+++ public/app.js
@@ -0,0 +1,28 @@
+// $(document).ready(function () {
+
+//     $("#scrapeBtn").on("click", function (event) {
+//         event.preventDefault();
+//         $.get("/newscrape", function (data) {
+//             location.reload();
+//         });
+//     });
+
+//     $("#id").on("click", function (event) {
+//         event.preventDefault();
+//         console.log("save it!");
+//         var id = $(this).val();
+       
+//         console.log(id);
+//         $.put("/update/" + id, function(data) {
+//             console.log(data)
+//             location.reload();
+//         })
+//     });
+// });
+
+
+
+
+
+
+
